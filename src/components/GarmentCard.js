90a0977f162c8b860951e61e75dd2d92d9d8669d
Justin Rodriguez <justin.r@me.com>
2024-03-02 14:09:25 -0500

    add css for GarmentCard.js

--- src/components/GarmentCard.js
+++ src/components/GarmentCard.js
@@ -1,8 +1,18 @@
+import './GarmentCard.css'
+
 function GarmentCard({garment}) {
+
     return (
         <div className="card">
           <img src={garment.image} alt="card" className="card-image" />
-          <div className="card-description">{garment.brand}</div>
+          <div className="card-description">
+            <ul>
+              <li>Brand: {garment.brand}</li>
+              <li>Category: {garment.category}</li>
+              <li>Size: {garment.size}</li>
+              <li>Color: {garment.color}</li>
+            </ul>
+          </div>
         </div>
       )
 }
